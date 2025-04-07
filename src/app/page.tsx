73038b1e0af47923944d3bd0d47a6286632156d0
jun <junhyeongjoen@junui-Macmini.local>
2025-04-07 19:04:00 +0900

    delete old version of chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Container from '@/components/candleChart/Container';
-import CandleChartContainer from '@/components/CandleChartContainer';
 
 export default function Home() {
   return (
--- src/app/ws/page.tsx
+++
@@ -1,9 +0,0 @@
-import CandleChartContainer from '@/components/CandleChartContainer';
-import BybitWS from '@/components/BybitWS';
-export default function page() {
-  return (
-    <div>
-      <CandleChartContainer />
-    </div>
-  );
-}
